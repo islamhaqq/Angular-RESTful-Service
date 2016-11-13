9f7b69b1da4febf597e9cb32ff9c378f79fbcdb9
Syed Aman <sna274@nyu.edu>
2016-11-13 10:22:02 -0500

    import map for observable

--- src/app/http-test.service.ts
+++ src/app/http-test.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+// import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class HttpTestService {
@@ -7,7 +9,8 @@ export class HttpTestService {
   constructor(private _http:Http) { }
 
   getCurrentTime() {
-    return this._http.get('http://date.jsontest.com/');
+    return this._http.get('http://date.jsontest.com/')
+      .map(res => res.json());
   }
 
   postJSON() {
