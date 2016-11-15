a62594740c4dbcadb3a6f0adbbb12e2ba4b6b7c4
Syed Aman <sna274@nyu.edu>
2016-11-15 08:22:06 -0500

    update template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'AngularJS RESTful Service';
   description = 'REST API obtained from http://www.jsontest.com/';
+  summary = 'Demonstrating implementation of Angular 2 HTTP Service to connect to a RESTful service and send GET and POST requests, as well as how to handle responses';
 }
